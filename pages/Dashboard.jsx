c0f2d7c90125f7eb7cf1a5c3369da8c28709005f
bryanJesus2003 <jesusbryan155@gmail.com>
2022-11-09 23:13:53 -0600

    myv2_A

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -3,48 +3,66 @@ import { Context } from "../context";
 import Loading from "../components/Loading";
 import FetchingAuth from "../fetching/authClass";
 import { authEndpoint } from "../endpoints/index";
-import { hasCookie, getCookies, deleteCookie } from "cookies-next";
-const authWorker = new FetchingAuth(authEndpoint);
 import { useRouter } from "next/router";
-import CardSubject from "../components/CardSubject"
+import CardSubject from "../components/CardSubject";
 
-import MathPicture from "../public/mathSubject.webp"
-import SpanishPicture from "../public/spanishSubject.jpg"
-import HistoryPicture from "../public/historySubject.jpg"
-import GeographyPicture from "../public/geograpySubject.jpg"
+import MathPicture from "../public/mathSubject.webp";
+import SpanishPicture from "../public/spanishSubject.jpg";
+import HistoryPicture from "../public/historySubject.jpg";
+import GeographyPicture from "../public/geograpySubject.jpg";
 
-const Dashboard = ({ data }) => {
+const Dashboard = ({ authData }) => {
   const { state, dispatch } = useContext(Context);
-   const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
-       dispatch({
-            type : "RELOADING",
-            payload : data
-       })
-       if(!state.loggedIn) router.push("/")
-  }, [data]);
+    if (authData.ok)
+      dispatch({
+        type: "RELOADING",
+        payload: authData,
+      });
+    else router.push("/");
+    if (!state.loggedIn) router.push("/");
+  }, [authData]);
 
   const GoSubject = (subject) => {
-       router.push(`/Dashboard/${subject}`) 
-  }
+    router.push(`/Dashboard/${subject}`);
+  };
 
   return (
-    <div className = "w-full  flex items-center flex-wrap justify-around p-5">
-         <CardSubject picture = {MathPicture} title = "Math Subject" value = {() => GoSubject("Math")}/>
-         <CardSubject picture = {SpanishPicture} title = "Spanish Subject" value = {() => GoSubject("Spanish")}/>
-         <CardSubject picture = {HistoryPicture} title = "History Subject" value = {() => GoSubject("History")}/>
-         <CardSubject picture = {GeographyPicture} title = "Geography Subject" value = {() => GoSubject("Geography")}/>
+    <div className="w-full  flex items-center flex-wrap justify-around p-5">
+      <CardSubject
+        picture={MathPicture}
+        title="Math Subject"
+        value={() => GoSubject("Math")}
+      />
+      <CardSubject
+        picture={SpanishPicture}
+        title="Spanish Subject"
+        value={() => GoSubject("Spanish")}
+      />
+      <CardSubject
+        picture={HistoryPicture}
+        title="History Subject"
+        value={() => GoSubject("History")}
+      />
+      <CardSubject
+        picture={GeographyPicture}
+        title="Geography Subject"
+        value={() => GoSubject("Geography")}
+      />
     </div>
   );
 };
 
 export const getServerSideProps = async ({ req, res }) => {
   try {
-    const response = await authWorker.getInfoJWT(req.cookies.userMonlech);
-    return { props: { data: response } };
+    const authWorker = new FetchingAuth(authEndpoint);
+    const responseAuth = await authWorker.getInfoJWT(req.cookies.userMonlech);
+
+    return { props: { authData: responseAuth } };
   } catch (error) {
-    return { props: { msg: null } };
+    return { props: { authData: { ok: false } } };
   }
 };
 
--- pages/Dashboard/[Subject].jsx
+++ pages/Dashboard/[Subject].jsx
@@ -1,33 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
+import { Context } from "../../context";
 import { useRouter } from "next/router";
-import { blogEndpoint } from "../../endpoints/index";
+import { blogEndpoint, authEndpoint } from "../../endpoints/index";
 import FetchingBlog from "../../fetching/blogClass";
+import FetchingAuth from "../../fetching/authClass";
 
-const Subject = ({ payload }) => {
+const Subject = ({ authData }) => {
+  const { state, dispatch } = useContext(Context);
   const router = useRouter();
 
+  /*
   useEffect(() => {
-      if(!payload.ok)
-            router.push("/Dashboard")
-  }, []);
-
+    if (authData.ok)
+      dispatch({
+        type: "RELOADING",
+        payload: authData,
+      });
+      console.log(state, "State here")
+    //if (!state.loggedIn) router.push("/");
+ //   if (!payload.ok) router.push("/Dashboard");
   
+  }, [authData]);*/
+
   return (
-      <div className="w-full">
-           <h2 className="font-bold text-3xl text-center">  Subject : {payload.data.subject} </h2>
-      </div>
-  )
+    <div className="w-full">
+      <h2 className="font-bold text-3xl text-center">
+        {" "}
+        {/*Subject : {payload.data.subject}{" "}*/}
+      </h2>
+    </div>
+  );
 };
 
-export const getServerSideProps = async (context) => {
+export const getServerSideProps = async ({req, res}) => {
   try {
-    const blogWorker = new FetchingBlog(blogEndpoint);
-    const response = await blogWorker.getSubjectData({
+    const authWorker = new FetchingAuth(authEndpoint);
+    //const blogWorker = new FetchingBlog(blogEndpoint);
+
+
+
+    const responseAuth = await authWorker.getInfoJWT(
+      req.cookies.userMonlech
+    );
+    /*const response = await blogWorker.getSubjectData({
       action: "GETALLSUBJECT",
       payload: context.params.Subject,
-    });
-    return { props: { payload: response } };
+    });*/
+    console.log(responseAuth, "Mine gdggg")
+    return { props: { authData: responseAuth, payload: {} } };
   } catch (error) {
+    console.log(error)
     return { props: { data: {} } };
   }
 };
