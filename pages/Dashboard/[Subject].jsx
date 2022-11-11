4bbaf0c2f16aa9a62be495f4e3182845f823e3a6
bryanJesus2003 <jesusbryan155@gmail.com>
2022-11-10 18:24:34 -0600

    myv2_B

--- pages/Dashboard/[Subject].jsx
+++ pages/Dashboard/[Subject].jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Context } from "../../context";
 import { useRouter } from "next/router";
 import { blogEndpoint, authEndpoint } from "../../endpoints/index";
@@ -7,20 +7,20 @@ import FetchingAuth from "../../fetching/authClass";
 
 const Subject = ({ authData }) => {
   const { state, dispatch } = useContext(Context);
+  const [myAuthData, setMyAuthData] = useState({});
   const router = useRouter();
 
-  /*
   useEffect(() => {
-    if (authData.ok)
-      dispatch({
-        type: "RELOADING",
-        payload: authData,
-      });
-      console.log(state, "State here")
-    //if (!state.loggedIn) router.push("/");
- //   if (!payload.ok) router.push("/Dashboard");
-  
-  }, [authData]);*/
+    if (authData.ok) setMyAuthData(authData);
+    else router.push("/");
+  }, []);
+
+  useEffect(() => {
+    dispatch({
+      type: "RELOADING",
+      payload: myAuthData,
+    });
+  }, [myAuthData]);
 
   return (
     <div className="w-full">
@@ -32,24 +32,19 @@ const Subject = ({ authData }) => {
   );
 };
 
-export const getServerSideProps = async ({req, res}) => {
+export const getServerSideProps = async ({ req, res }) => {
   try {
     const authWorker = new FetchingAuth(authEndpoint);
     //const blogWorker = new FetchingBlog(blogEndpoint);
 
-
-
-    const responseAuth = await authWorker.getInfoJWT(
-      req.cookies.userMonlech
-    );
+    const responseAuth = await authWorker.getInfoJWT(req.cookies.userMonlech);
     /*const response = await blogWorker.getSubjectData({
       action: "GETALLSUBJECT",
       payload: context.params.Subject,
     });*/
-    console.log(responseAuth, "Mine gdggg")
     return { props: { authData: responseAuth, payload: {} } };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return { props: { data: {} } };
   }
 };
